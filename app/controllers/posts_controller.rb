e5269e70a318eb7cfdd08a6b736f9aaf6367294c
cmharada <haradacm@gmail.com>
2014-08-29 14:10:30 -0700

    Finished phase 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :ensure_author_is_current_user, only: [:edit, :update, :destroy]
+  before_action :ensure_user_logged_in, only: [:new, :create]
+  
   def new
     @post = Post.new
     @subs = Sub.all
@@ -18,9 +21,20 @@ class PostsController < ApplicationController
   end
   
   def edit
+    @post = Post.find(params[:id])
+    @subs = Sub.all
+    @current_sub = Sub.find(params[:sub_id])
+    render :edit
   end
   
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to sub_post_url(@post.sub, @post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :edit
+    end
   end
   
   def show
@@ -30,9 +44,26 @@ class PostsController < ApplicationController
   end
   
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to @post.sub
   end
   
   private
+  
+  def ensure_author_is_current_user
+    unless Post.find(params[:id]).author == current_user
+      flash[:errors] = ["You did not author this post"]
+      redirect_to sub_url(Post.find(params[:id]).sub)
+    end
+  end
+  
+  def ensure_user_logged_in
+    unless current_user
+      flash[:errors] = ["You must log in to post"]
+      redirect_to new_session_url
+    end
+  end
   
   def post_params
     params.require(:post).permit(:title, :url, :content, :sub_id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resource :session, only: [:new, :create, :destroy]
   resources :subs do
-    resources :posts, only: [:new, :edit, :update, :destroy, :show]
+    resources :posts, only: [:new, :edit, :destroy, :show]
   end
-  resources :posts, only: [:create]
+  resources :posts, only: [:create, :update]
 end
