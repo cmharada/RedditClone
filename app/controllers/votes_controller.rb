0a81964d0d357e7e17722d7fb1ace45afeb81ac9
cmharada <haradacm@gmail.com>
2014-08-29 17:57:51 -0700

    End of day

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,11 @@ class VotesController < ApplicationController
   def create
     @vote = Vote.new(vote_params)
     if @vote.save
-      redirect_to subs_url
+      if @vote.votable_type == "Post"
+        redirect_to post_url(@vote.votable.id)
+      else
+        redirect_to post_url(@vote.votable.post_id)
+      end
     else
       flash[:errors] = @vote.errors.full_messages
       redirect_to subs_url
